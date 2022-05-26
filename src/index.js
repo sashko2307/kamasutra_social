3206c76b6e100a4ffc91327ff1e8ca292d3eb8b4
sashko ss <sashko2307@gmail.com>
2022-05-26 20:19:48 +0300

    r

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+//
 //import { addPost, updateNewPostText } from './redux/state'
 import { store } from './redux/store';
 
