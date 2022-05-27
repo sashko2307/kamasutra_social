960b1cf43aff8b2bf207400c09ab10b39ba1d5a3
sashko ss <sashko2307@gmail.com>
2022-05-27 15:24:49 +0300

    42 конец

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 //import { addPost, updateNewPostText } from './redux/state'
-import { store } from './redux/store';
+import store from './redux/redux-store';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -17,7 +17,10 @@ const renderEntireTree = (state) => {
 	);
 }
 renderEntireTree(store.getState())
-store.sibscriber(renderEntireTree)
+store.subscribe(() => {
+	let state = store.getState()
+	renderEntireTree(state)
+})
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
